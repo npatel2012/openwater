823cdae0d543c2f5d448023c9789493bc20fa0f6
npatel2012 <npatel@redhat.com>
2015-06-17 17:00:07 +0100

    giving base structure to the demo projec

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-#Instructions go here
-Hello
+#Instructions to follow
+
+1. git clone this project using following command on local machine:
+	git clone https://github.com/npatel2012/openwater.git
+
+2. Download and add following binaries from Red Hat customer portal to installs folder
+	- JBoss EAP 6.4 installer: jboss-eap-6.4.0-installer.jar
+	- JBoss BPMS 6.1.0 installer:i jboss-bpmsuite-6.1.0.GA-installer.jar
+
+3. Run init.sh setup script using following command
+	./init.sh
+
+4. Start BPMS instance using following command
+	./target/jboss-eap-6.4/bin/standalone.sh	
+
+5. Login to business-central via
+	http://localhost:8080/business-central (u:adminuser/p:bpmsuite1!)
+
+
+You are ready to go now!!!
---
+++ init.sh
@@ -0,0 +1,157 @@
+#!/bin/sh 
+DEMO="JBoss BPM Suite Openwater Demo"
+AUTHORS="Niraj Patel, Iain Boyle, Martin Swales"
+PROJECT="git@github.com:npatel2012/openwater.git"
+
+#BPM environment variables
+JBOSS_HOME=./target/jboss-eap-6.4
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
+SRC_DIR=./installs
+PRJ_DIR=./projects
+SUPPORT_DIR=./support
+BPMS=jboss-bpmsuite-6.1.0.GA-installer.jar
+EAP=jboss-eap-6.4.0-installer.jar
+BPM_VERSION=6.1
+
+# wipe screen.
+clear 
+
+# add executeable in installs
+chmod +x installs/*.zip
+
+echo
+echo "#####################################################################################"
+echo "##                                                                                 ##"   
+echo "##  Setting up the                                                                 ##"
+echo "##                                                                                 ##"   
+echo "##            ${DEMO}                ##"
+echo "##                                                                                 ##"   
+echo "##                                                                                 ##"   
+echo "##        ####   ####    #   #    ###             ####  #  #   ###  ####           ##"
+echo "##        #   #  #   #  # # # #  #         #      #     #  #  #     #              ##"
+echo "##        ####   ####   #  #  #   ##      ###     ###   #  #   ##   ###            ##"
+echo "##        #   #  #      #     #     #      #      #     #  #     #  #              ##"
+echo "##        ####   #      #     #  ###              #     ####  ###   ####           ##"
+echo "##                                                                                 ##"   
+echo "##                                                                                 ##"   
+echo "##  brought to you by,                                                             ##"   
+echo "##                     ${AUTHORS}                             ##"
+echo "##                                                                                 ##"   
+echo "##  ${PROJECT}   ##"
+echo "##                                                                                 ##"   
+echo "#####################################################################################"
+echo
+
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+# make some checks first before proceeding.	
+if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
+	echo Product sources are present...
+	echo
+else
+	echo Need to download $EAP package from the Customer Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
+	echo Product sources BPM are present...
+	echo
+else
+	echo Need to download $BPMS package from the Customer Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+
+# Remove JBoss product installation if exists.
+if [ -x target ]; then
+	echo "  - existing JBoss product installation detected..."
+	echo
+	echo "  - removing existing JBoss product installation..."
+	echo
+	rm -rf target
+fi
+
+# Run installers.
+echo "JBoss EAP installer running now..."
+echo
+java -jar $SRC_DIR/$EAP $SUPPORT_DIR/installation-eap -variablefile $SUPPORT_DIR/installation-eap.variables
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during JBoss EAP installation!
+	exit
+fi
+
+echo "JBoss BPM Suite installer running now..."
+echo
+java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
+
+if [ $? -ne 0 ]; then
+	echo Error occurred during BPMS installation!
+	exit
+fi
+
+
+echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo
+cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+
+echo "  - setting up demo projects..."
+echo
+cp -r $SUPPORT_DIR/bpm-suite-demo-niogit $SERVER_BIN/.niogit
+
+
+echo "  Building maven artifacts..."
+echo
+mvn clean install -f $PRJ_DIR/pom.xml
+
+
+echo
+echo "  - deploying external-client-ui-form-1.0.war to EAP deployments directory"
+cp -r $PRJ_DIR/external-client-ui-form/target/external-client-ui-form-1.0.war $SERVER_DIR/
+
+echo "  - setting up standalone.xml configuration adjustments..."
+echo
+cp $SUPPORT_DIR/standalone.xml $SERVER_CONF
+
+echo "  - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo "  - setup email task notification users..."
+echo
+cp $SUPPORT_DIR/userinfo.properties $SERVER_DIR/business-central.war/WEB-INF/classes/
+
+echo "  - updating the CustomWorkItemHandler.conf file to use the appropriate email server..."
+echo
+cp -f $SUPPORT_DIR/CustomWorkItemHandlers.conf $SERVER_DIR/business-central.war/WEB-INF/classes/META-INF
+
+
+# Optional: uncomment this to install mock data for BPM Suite.
+#
+#echo - setting up mock bpm dashboard data...
+#cp $SUPPORT_DIR/1000_jbpm_demo_h2.sql $SERVER_DIR/dashbuilder.war/WEB-INF/etc/sql
+#echo
+
+
+
+echo
+echo "==========================================================================================="
+echo "=                                                                                         ="
+echo "=  You can now start the JBoss BPM Suite with:                                            ="
+echo "=                                                                                         ="
+echo "=        $SERVER_BIN/standalone.sh                                         ="
+echo "=                                                                                         ="
+echo "=    - login, build and deploy JBoss BPM Suite process project at:                        ="
+echo "=                                                                                         ="
+echo "=        http://localhost:8080/business-central (u:adminuser/p:bpmsuite1!)                    ="
+echo "=                                                                                         ="
+echo "=   $DEMO Setup Complete.                 ="
+echo "==========================================================================================="
+echo
